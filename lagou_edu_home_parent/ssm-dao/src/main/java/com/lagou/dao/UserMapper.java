1164899acd5afcb2d83e8ac6da8ed377476b302e
huang <729971144@qq.com>
2023-05-22 14:17:04 +0800

    update GitMapper

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -25,4 +25,12 @@ public interface UserMapper {
     public List<Resource> findResourceByRole(List<Integer> ids);
     
     public List<Resource> findResourceByRole2(List<Integer> ids);
+
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
 }
